778301245770871c9ec99f99a455bf13587a64e5
alejes <alejes@yandex.ru>
2015-12-29 03:18:00 +0300

    separate tests

--- src/test/java/audb/BTreeTest.java
+++ src/test/java/audb/BTreeTest.java
@@ -3,7 +3,6 @@ package audb;
 import audb.command.*;
 import audb.command.Constraint.ConstraintType;
 import audb.index.Index.Order;
-import audb.page.PageStructure;
 import audb.parser.Parser;
 import audb.table.*;
 import audb.type.IntegerType;
@@ -59,103 +58,7 @@ public class BTreeTest extends TestCase {
 
 	}
 
-	public void testInsertWithoutIndex() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
 
-
-		parser.getCommand("CREATE TABLE tableload4m (number VARCHAR (15), text VARCHAR (9))").exec();
-
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("INSERT INTO tableload4m (number, text) VALUES ('%09d', 'sadfsd')", i)).exec();
-			if (i % 10000 == 25) {
-				System.out.println("Insert " + i + " items");
-			}
-		}
-
-		PageStructure.flush();
-	}
-
-	public void testInsertWithPreIndex() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
-
-		parser.getCommand("CREATE TABLE tableload4mpreindex (number VARCHAR (15), text VARCHAR (9))").exec();
-		parser.getCommand("CREATE UNIQUE INDEX indexname ON tableload4mpreindex (number DESC) USING BTREE;").exec();
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("INSERT INTO tableload4mpreindex (number, text) VALUES ('%09d', 'sadfsd')", i)).exec();
-			if (i % 10000 == 25) {
-				System.out.println("Insert " + i + " items");
-			}
-		}
-
-		PageStructure.flush();
-	}
-
-	public void testInsertWithPostIndex() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
-
-		parser.getCommand("CREATE TABLE tableload4mpostindex (number VARCHAR (15), text VARCHAR (9))").exec();
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("INSERT INTO tableload4mpostindex (number, text) VALUES ('%09d', 'sadfsd')", i)).exec();
-			if (i % 10000 == 25) {
-				System.out.println("Insert " + i + " items");
-			}
-		}
-		parser.getCommand("CREATE UNIQUE INDEX indexname ON tableload4mpostindex (number DESC) USING BTREE;").exec();
-
-		PageStructure.flush();
-	}
-
-	public void testInsertWithoutIndexSpeedSelect() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("SELECT * FROM tableload4m WHERE number = '%09d'", random.nextInt(120_000_0))).exec();
-		}
-
-		PageStructure.flush();
-	}
-
-	public void testInsertWithPreIndexSpeedSelect() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("SELECT * FROM tableload4mpreindex WHERE number = '%09d'", random.nextInt(120_000_0))).exec();
-		}
-
-		PageStructure.flush();
-	}
-
-	public void testInsertWithPostIndexSpeedSelect() throws Exception {
-		assertTrue(true);
-		Parser parser = new Parser();
-		TableManager tableManager = new TableManager();
-		Command.setTableManager(tableManager);
-
-		for (int i = 0; i < 100_00; i++) {
-			parser.getCommand(String.format("SELECT * FROM tableload4mpostindex WHERE number = '%09d'", random.nextInt(120_000_0))).exec();
-		}
-
-		PageStructure.flush();
-	}
-	
 	public void testCreateIndex() throws Exception {
 		Parser parser = new Parser();
 		TableManager tableManager = new TableManager();
